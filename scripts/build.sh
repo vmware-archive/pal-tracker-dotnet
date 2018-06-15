3589f1476db8269be75a3de4c7c1eab3ff71d4a9
Mike Gehard <mgehard@pivotal.io>
2018-06-15 10:40:04 -0600

    Add deployment pipeline

---
+++ scripts/build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -ex
+
+DISABLE_AUTH=true dotnet test ./test/PalTrackerTests
+artifacts_path=/tmp/artifacts
+
+build_output="/tmp/build-output"
+artifacts_path="./artifacts"
+version=$1
+
+mkdir -p $build_output
+mkdir -p $artifacts_path
+
+cp manifest-*.yml $build_output
+
+dotnet publish src/PalTracker --configuration Release \
+    --output $build_output/src/PalTracker/bin/Release/netcoreapp3.1/publish
+
+tar -C $build_output/ -cvzf $artifacts_path/pal-tracker-$version.tgz .
